527df51535c0c41508dc6549b802d1a7a7158582
Carlos <carlos.rodil93@gmail.com>
2016-09-16 20:17:00 -0500

    1

--- CoreImageDemo/ViewController.swift
+++ CoreImageDemo/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var foto: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func original(_ sender: AnyObject) {
+        foto.image = UIImage(named: "aa.jpg")
+    }
+    @IBAction func gris(_ sender: AnyObject) {
+        let image = UIImage(named: "aa.jpg")
+        let originalImage = CIImage(image: image!)
+        
+        // 2
+        let filter = CIFilter(name: "CIPhotoEffectMono")
+        filter!.setDefaults()
+        filter!.setValue(originalImage, forKey: kCIInputImageKey)
+        
+        // 3
+        let outputImage = filter!.outputImage
+        let newImage = UIImage(ciImage: outputImage!)
+        foto.image = newImage
+    }
 
 }
 
